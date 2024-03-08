b1c0850debf7b73fb61936b6000bc147853975c6
yogesh-stack-dev <yogesh.devoloper@gmail.com>
2024-03-08 14:22:50 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        Nutmeg: '#854632',
+        DarkRaspberry: '#7b284f',
+        customWhite: '#ffffff',
+        RoseWhite: '#fff5fa',
+        EggShell: '#f3e6d8',
+        LightGrey: '#e4ded8',
+        WengeBrown: '#5f574e',
+        DarkCharcoal: '#302d2c',
+        attibution:'#3f59a6',
+      }
+    },
+  },
+  plugins: [],
+}
+
